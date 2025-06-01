ef9b88d1218f203e6418634a21323905e777e9b6
NadiaE-Fernandez <naniifer98@gmail.com>
2025-05-31 21:04:19 -0300

    agrego datos-alumnos

---
+++ tp5/src/data/alumnos.js
@@ -0,0 +1,11 @@
+export const alumnos = [
+    {
+        lu:"APU00999",
+        nombre:"Maria Eugenia",
+        apellido: "Diaz",
+        curso: "Tercero",
+        email: "mariadiaz@gmail.com",
+        domicilio: "Av. Congreso 125",
+        telefono: "3886489657",
+    }
+]
