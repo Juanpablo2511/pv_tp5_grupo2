51ef8db4286ebd11fb9952cade281576ee549d8a
NadiaE-Fernandez <naniifer98@gmail.com>
2025-05-31 21:08:41 -0300

    muestra de ListaAlumnos

--- tp5/src/App.jsx
+++ tp5/src/App.jsx
@@ -1,7 +1,8 @@
-import { Routes, Route } from "react-router-dom"
-import Home from "./pages/Home"
-import Acerca from "./pages/Acerca"
-import NavBar from "./components/NavBar"
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import ListaAlumnos from "./pages/ListaAlumnos";
+import Acerca from "./pages/Acerca";
+import NavBar from "./components/NavBar";
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
       <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/alumnos" element={<ListaAlumnos />} />
         <Route path="/acerca" element={<Acerca />} />
       </Routes>
     </div>
