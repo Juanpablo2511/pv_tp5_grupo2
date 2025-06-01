ea716ac14fc36004167f14b9413e08732929debd
NadiaE-Fernandez <naniifer98@gmail.com>
2025-06-01 12:38:51 -0300

    centralizo estado de alumnos en app.jsx

--- tp5/src/App.jsx
+++ tp5/src/App.jsx
@@ -1,3 +1,4 @@
+import React , {useState} from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import ListaAlumnos from "./pages/ListaAlumnos";
@@ -5,12 +6,56 @@ import Acerca from "./pages/Acerca";
 import NavBar from "./components/NavBar";
 
 export default function App() {
+  const [alumnos,setAlumnos] = useState([
+    {
+        lu:"APU00999",
+        nombre:"Maria Eugenia",
+        apellido: "Diaz",
+        curso: "Tercero",
+        email: "mariadiaz@gmail.com",
+        domicilio: "Av. Congreso 125",
+        telefono: "3886489657",
+    },
+    {
+        lu:"APU001",
+        nombre:"Nadia Evangelina",
+        apellido: "Fernandez",
+        curso: "Tercero",
+        email: "nadia@gmail.com",
+        domicilio: "Av.Peron 120",
+        telefono: "3886980987",
+    },
+    {
+        lu:"APU002",
+        nombre:"Juan Pablo",
+        apellido: "Farfan",
+        curso: "Tercero",
+        email: "juan@gmail.com",
+        domicilio: "Av. ",
+        telefono: "3885123956",
+    },
+    {
+        lu:"APU003",
+        nombre:"Matias",
+        apellido: "Quispe",
+        curso: "Tercero",
+        email: "matias@gmail.com",
+        domicilio: "Av. ",
+        telefono: "3885984372",
+    },
+    
+
+  ])
+
+  const eliminarAlumno = (lu) => {
+    setAlumnos(alumnos.filter(alumno => alumno.lu !== lu));
+  }
   return (
     <div>
       <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/alumnos" element={<ListaAlumnos />} />
+        <Route path="/alumnos" element={<ListaAlumnos alumnos={alumnos} eliminarAlumno={eliminarAlumno}/>} />
         <Route path="/acerca" element={<Acerca />} />
       </Routes>
     </div>
--- tp5/src/pages/ListaAlumnos.jsx
+++ tp5/src/pages/ListaAlumnos.jsx
@@ -1,20 +1,20 @@
 import React , {useState} from "react";
 import AlumnoCard from "../components/AlumnoCard";
-import { alumnos as alumnosData } from "../data/alumnos";
 
-const ListaAlumnos = () => {
-    const [alumnos,setAlumnos]= useState (alumnosData);
+
+const ListaAlumnos = ({alumnos, eliminarAlumno }) => {
     
-    const handleDelete = (lu) =>{
-        setAlumnos(alumnos.filter(alumno => alumno.lu !==lu));
-    }
-
     return (
         <div>
             <h2>Lista de Alumnos</h2>
-            {alumnos.map(alumno => (
-                <AlumnoCard key={alumno.lu} alumno={alumno} onDelete={handleDelete} />
-            ))}
+            {alumnos.length === 0 ? (
+                <p>No hay alumnos registrados</p> ) : (
+                    alumnos.map(alumno => (
+                        <AlumnoCard key={alumno.lu} alumno={alumno} onDelete={eliminarAlumno} />
+                ))
+            )}
+            
+        
         </div>
     )
 }
